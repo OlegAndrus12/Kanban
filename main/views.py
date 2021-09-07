80b2fdfec4b9be95effcb41576c5b948cfc6d3dd
OlegAndrus12 <andrusoleg123@gmail.com>
2021-09-07 22:31:30 +0300

    Sidebar is drawn

--- main/views.py
+++ main/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 from main.models import Task, TaskBar
 
 def index(response):
-    tasks = list(Task.objects.all())
-    print(tasks)
-    return render(response, "main/taskBar.html", {"tasks": tasks})
+    return render(response, "main/base.html")
 
 def login(response):
     return render(response, "main/login.html", {})
+
+def notes(response):
+    tasks = list(Task.objects.all())
+    print(tasks)
+    return render(response, "main/taskBar.html", {"tasks": tasks})
